c4872c07765152efed2b39077152d67fff766c31
Rick van den Bosch <rickvdbosch@outlook.com>
2020-09-17 05:13:19 +0200

    Implemented creating Storage Account and App Configuration examples (#4)
    * Fixed typo
    Removed unused using
    
    * Fixed typo in create-virtual-machine.md
    
    * Fixed typo in manage-virtual-machine readme.md
    
    * Fixed typo in manage-virtual-machine-extension readme.md
    
    * Fixed typo in manage-ip-address readme.md
    
    * Removed unused field formats
    Simplified String to string
    Initialized string with string.Empty instead of magic string ""
    
    * Replaced magic string "" with string.Empty
    Removed useless call to armTemplateString.IndexOf
    Simplified String -> string
    
    * Updated Azure.Identity to version 1.2.2
    
    * Updated the Azure.Messaging.EventHubs NuGet package
    (since the PR is still open and there's a new version available now 😁)
    
    * Implemented PrintStorageAccount
    
    * Added links to all existing and new samples
    
    * Added example for creating a Storage Account
    
    * Fixed location of CreateStorageExample
    
    * Added CreateAppConfigurationSample
    
    * Implemented PrintAppConfiguration
    
    * Implemented Create App Configuration example

---
+++ samples/appconfiguration/create-app-configuration/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Azure.Identity;
+using Azure.ResourceManager.AppConfiguration;
+using Azure.ResourceManager.AppConfiguration.Models;
+
+using Samples.Utilities;
+
+namespace CreateAppConfigurationSample
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            try
+            {
+                // Authenticate
+                var credential = new DefaultAzureCredential();
+
+                await RunSample(credential);
+            }
+            catch (Exception ex)
+            {
+                Utilities.Log(ex);
+            }
+        }
+
+        public static async Task RunSample(DefaultAzureCredential credential)
+        {
+            string subscriptionId = Environment.GetEnvironmentVariable("AZURE_SUBSCRIPTION_ID");
+            string appConfigName = Utilities.RandomResourceName("appcnfg", 20);
+            string rgName = Utilities.RandomResourceName("rgNEMV", 24);
+            string region = "eastus";
+
+            var appConfigManagementClient = new AppConfigurationManagementClient(subscriptionId, credential);
+            var configurationStores = appConfigManagementClient.ConfigurationStores;
+
+            await ResourceGroupHelper.CreateOrUpdateResourceGroup(rgName, region);
+
+            // Create an App Configuration
+            Utilities.Log("Creating an App Configuration...");
+
+            var configurationStore = new ConfigurationStore(region, new Sku("free"));
+
+            var rawResult = await configurationStores.StartCreateAsync(rgName, appConfigName, configurationStore);
+            var appConfiguration = (await rawResult.WaitForCompletionAsync()).Value;
+
+            Utilities.Log("Created App Configuration");
+            Utilities.PrintAppConfiguration(appConfiguration);
+        }
+    }
+}
