5ce5f2fc3c86170559fc087589b1b7262b83d5cb
mcgallan <1186003288@qq.com>
2023-12-18 15:59:06 +0800

    Update

--- samples/resources/deploy-using-arm-template/Program.cs
+++ samples/resources/deploy-using-arm-template/Program.cs
@@ -3,6 +3,7 @@
 
 using Azure.Core;
 using Azure.Identity;
+using Azure.ResourceManager;
 using Azure.ResourceManager.Resources;
 using Azure.ResourceManager.Resources.Models;
 using Samples.Utilities;
@@ -13,44 +14,39 @@ namespace DeployUsingARMTemplate
 {
     public class Program
     {
-        public static async Task RunSample(TokenCredential credential)
+        public static async Task RunSample(ArmClient client)
         {
             var rgName = Utilities.RandomResourceName("rgRSAT", 24);
             var deploymentName = Utilities.RandomResourceName("dpRSAT", 24);
-            var location = "westus";
+            var location = AzureLocation.WestUS;
             var subscriptionId = Environment.GetEnvironmentVariable("AZURE_SUBSCRIPTION_ID");
             var templateJson = Utilities.GetArmTemplate("ArmTemplate.json");
 
-            var resourceClient = new ResourcesManagementClient(subscriptionId, credential);
-            var resourceGroups = resourceClient.ResourceGroups;
-            var deployments = resourceClient.Deployments;
+            // Create resource group.
+
+            Utilities.Log("Creating a resource group with name: " + rgName);
+            ResourceGroupResource resourceGroup = (await client.GetDefaultSubscription().GetResourceGroups().CreateOrUpdateAsync(Azure.WaitUntil.Completed, rgName, new ResourceGroupData(location))).Value;
+            Utilities.Log("Created a resource group with name: " + rgName);
 
             try
             {
-                // Create resource group.
-
-                Utilities.Log("Creating a resource group with name: " + rgName);
-
-                var resourceGroup = new ResourceGroup(location);
-                resourceGroup = await resourceGroups.CreateOrUpdateAsync(rgName, resourceGroup);
-
-                Utilities.Log("Created a resource group with name: " + rgName);
-
                 // Create a deployment for an Azure App Service via an ARM
                 // template.
 
                 Utilities.Log("Starting a deployment for an Azure App Service: " + deploymentName);
 
-                var parameters = new Deployment
+                var deployMentCollection = resourceGroup.GetArmDeployments();
+
+                var deployMentData = new ArmDeploymentContent
                 (
-                    new DeploymentProperties(DeploymentMode.Incremental)
+                    new ArmDeploymentProperties(ArmDeploymentMode.Incremental)
                     {
-                        Template = templateJson,
-                        Parameters = "{}"
+                        Template = BinaryData.FromObjectAsJson(templateJson),
+                        Parameters = BinaryData.FromString("\"{}\"")
                     }
                  );
-                var rawResult = await deployments.StartCreateOrUpdateAsync(rgName, deploymentName, parameters);
-                await rawResult.WaitForCompletionAsync();
+                var rawResult = await deployMentCollection.CreateOrUpdateAsync(Azure.WaitUntil.Completed, deploymentName, deployMentData);
+                var deployMent = rawResult.Value;
 
                 Utilities.Log("Completed the deployment: " + deploymentName);
             }
@@ -60,7 +56,7 @@ namespace DeployUsingARMTemplate
                 {
                     Utilities.Log("Deleting Resource Group: " + rgName);
 
-                    await (await resourceGroups.StartDeleteAsync(rgName)).WaitForCompletionAsync();
+                    await resourceGroup.DeleteAsync(Azure.WaitUntil.Completed);
 
                     Utilities.Log("Deleted Resource Group: " + rgName);
                 }
@@ -76,9 +72,14 @@ namespace DeployUsingARMTemplate
             try
             {
                 // Authenticate
-                var credentials = new DefaultAzureCredential();
+                var clientId = Environment.GetEnvironmentVariable("CLIENT_ID");
+                var clientSecret = Environment.GetEnvironmentVariable("CLIENT_SECRET");
+                var tenantId = Environment.GetEnvironmentVariable("TENANT_ID");
+                var subscription = Environment.GetEnvironmentVariable("SUBSCRIPTION_ID");
+                ClientSecretCredential credential = new ClientSecretCredential(tenantId, clientId, clientSecret);
+                ArmClient client = new ArmClient(credential, subscription);
 
-                await RunSample(credentials);
+                await RunSample(client);
             }
             catch (Exception ex)
             {
